0dd92cad4e92a67cd4f9c937cd9ba4c7253375fb
aruizgg <antoniorzgar@gmail.com>
2025-02-16 12:57:45 +0100

    Implementación de createNewBlock + validateBlock

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"time"
 )
 
 type Block struct {
@@ -19,4 +20,27 @@ func calculateHash(block Block) string {
 	hash.Write([]byte(concBlock))
 	hashed := hash.Sum(nil)
 	return hex.EncodeToString(hashed)
+}
+
+func createNewBlock(oldBlock Block, data string) (Block, error) {
+	var newBlock Block
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = time.Now().String()
+	newBlock.Data = data
+	newBlock.Hash = calculateHash(newBlock)
+	newBlock.PrevHash = oldBlock.Hash
+
+	return newBlock, nil
+}
+
+func validateBlock(block Block) bool {
+
+	if block.Index == 0 { // Genesis Block has no prevBlock
+		return calculateHash(block) == block.Hash
+	} else {
+		// TODO: Assign block to prevBlock
+		var prevBlock Block
+
+		return calculateHash(block) == block.Hash && prevBlock.Hash == block.PrevHash
+	}
 }
